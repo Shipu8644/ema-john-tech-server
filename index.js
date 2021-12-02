7318c1ce944a0d290c5d747ca897f2f365102497
Shipu8644 <shipu.monjurur@gmail.com>
2021-12-03 03:46:43 +0600

    new product base64 image Api added

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express()
 const cors = require('cors')
 const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config()
+const fileUpload = require('express-fileupload');
 const { MongoClient } = require('mongodb');
 const port = process.env.PORT || 5000
 
 // middleWare
 app.use(cors())
 app.use(express.json())
+app.use(fileUpload())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.om5y9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -20,17 +22,47 @@ async function run() {
         const database = client.db("ema_tech");
         const productsCollection = database.collection("products");
 
+        //getting all products Api
         app.get('/products', async (req, res) => {
             const cursor = productsCollection.find({});
             const products = await cursor.toArray();
             res.json(products);
         })
+        //getting single product Api by Id
         app.get('/products/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const product = await productsCollection.findOne(query);
             res.json(product);
         })
+
+        //added a new product Api
+        app.post('/products', async (req, res) => {
+            const category = req.body.category;
+            const price = req.body.price;
+            const star = req.body.star;
+            const stock = req.body.stock;
+            const seller = req.body.seller;
+            const starCount = req.body.starCount;
+            const name = req.body.name;
+            const pic = req.files.image;
+            const picData = pic.data;
+            const encodedPic = picData.toString('base64');
+            const imageBuffer = Buffer.from(encodedPic, 'base64');
+            const product = {
+                category,
+                price,
+                star,
+                stock,
+                seller,
+                starCount,
+                name,
+                img: imageBuffer
+            }
+            const result = await productsCollection.insertOne(product);
+            res.json(result);
+        })
+
 
 
     } finally {
